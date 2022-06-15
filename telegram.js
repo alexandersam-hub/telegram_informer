29d047a311c914e2e8d19de436c16a643c7ce368
stan1i <alexandersamedov@gmail.com>
2022-06-15 10:57:34 +0300

    first commit v1

--- telegram.js
+++ telegram.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const TelegramService = require('./services/TelegramService')
 
 
-const PORT = process.env.PORT || 8004
+const PORT = process.env.PORT || 8005
 const app = express()
 
 const start = async ()=>{
